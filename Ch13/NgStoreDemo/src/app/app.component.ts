8b81fc4b283d43d3a5db4eaa64bb34bdf921a225
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2018-08-19 20:06:53 +0300

    Moved to Angular6 and @ngrx/store usage.

--- Ch13/NgStoreDemo/src/app/app.component.ts
+++ Ch13/NgStoreDemo/src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { Observable } from "rxjs";
+import { select, Store } from "@ngrx/store";
+import { INCREMENT, DECREMENT } from "./reducer";
 
 @Component({
   selector: 'app-root',
   template: `<button (click)="increment()">Increment</button>
   <button (click)="decrement()">Decrement</button>
-  <p>The counter: {{counter}}</p>`
+  <p>The counter: {{counter$ | async}}</p>`
 })
+
 export class AppComponent {
-  public counter: number = 0;
+  public counter$: Observable<number>;
+
+  constructor(private store: Store<any>) {
+    // "Subscribe" to the obervable returned by the Store.select() function.
+    this.counter$ = store.select("counterState");
+  }
 
   increment() {
-    this.counter++;
+    this.store.dispatch({ type: INCREMENT });
   }
 
   decrement() {
-    this.counter--;
+    this.store.dispatch({ type: DECREMENT });
   }
 }
--- Ch13/NgStoreDemo/src/app/app.module.ts
+++ Ch13/NgStoreDemo/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { counterReducer } from "./reducer";
+import { StoreModule } from "@ngrx/store";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Configure the app-level store. The property for the counter is named counterState.
+    StoreModule.forRoot({ counterState: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
